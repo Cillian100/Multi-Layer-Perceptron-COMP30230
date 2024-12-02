f85768ec158c5df7fe57c26074d32b17b0f5350e
cill <cillian.quinn@ucdconnect.ie>
2024-12-02 13:32:45 +0000

    Dec 2

--- multiLayerPerceptron.py
+++ multiLayerPerceptron.py
@@ -1,75 +1,89 @@
 import numpy as np
+import random as rand
 
 class MLP:
-    def __init__(self, input_size, hidden_size, output_size):
-        self.weights_input_hidden = np.random.randn(input_size, hidden_size)
-        self.weights_hidden_output = np.random.randn(hidden_size, output_size)
-        self.bias_hidden = np.zeros((1, hidden_size))
-        self.bias_output = np.zeros((1, output_size))
+    def __init__(self, input_size, hidden_sizes, output_size):
+        self.input_size = input_size
+        self.hidden_sizes = hidden_sizes
+        self.output_size = output_size
+        self.num_layers = len(hidden_sizes) + 1
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
+        # Initialize the weights and biases for each layer
+        self.weights = []
+        self.biases = []
+        sizes = [input_size] + hidden_sizes + [output_size]
+        for i in range(1, self.num_layers + 1):
+            self.weights.append(np.random.randn(sizes[i], sizes[i-1]))
+            self.biases.append(np.random.randn(sizes[i], 1))
 
-    def softmax(self, x):
-        exp_x = np.exp(x - np.max(x))
-        return exp_x / exp_x.sum(axis=1, keepdims=True)
+    def backward(self, X, y):
+        m = X.shape[1]
+        #m = X.shape
+        gradients = []
+        dZ = self.activations[-1] - y
+        for i in range(self.num_layers - 1, -1, -1):
+            dW = (1 / m) * np.dot(dZ, self.activations[i].T)
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+            gradients.append((dW, db))
 
-    def backward(self, X, y, output, learning_rate):
-        output_error = output - y
-        hidden_error = np.dot(output_error, self.weights_hidden_output.T) * self.hidden_output * (
-                    1 - self.hidden_output)
+            if i > 0:
+                dA = np.dot(self.weights[i].T, dZ)
+                dZ = dA * self.gradient_tanh(self.z[i - 1])
 
-        self.weights_hidden_output -= learning_rate * np.dot(self.hidden_output.T, output_error)
-        self.bias_output -= learning_rate * np.sum(output_error, axis=0, keepdims=True)
-        self.weights_input_hidden -= learning_rate * np.dot(X.T, hidden_error)
-        self.bias_hidden -= learning_rate * np.sum(hidden_error, axis=0, keepdims=True)
+        return gradients[::-1]
 
     def forward(self, X):
-        self.hidden_input = np.dot(X, self.weights_input_hidden) + self.bias_hidden
-        self.hidden_output = self.sigmoid(self.hidden_input)
-        self.final_input = np.dot(self.hidden_output, self.weights_hidden_output) + self.bias_output
-        self.final_output = self.softmax(self.final_input)
-        return self.final_output
+        self.activations = [X]
+        self.z = []
+        for i in range(self.num_layers):
+            z = np.dot(self.weights[i], self.activations[i]) + self.biases[i]
+            self.z.append(z)
+            if i < self.num_layers - 1:
+                a = self.tanh(z)
+            else:
+                a = z
+            self.activations.append(a)
+        return self.activations[-1]
 
-    def train(self, X, y, epochs, learning_rate):
-        for epoch in range(epochs):
-            output = self.forward(X)
-            self.backward(X, y, output, learning_rate)
-            if(epoch+1)%100 == 0:
-                loss = -np.sum(y * np.log(output)) / X.shape[0]
-                print(f'Epoch {epoch+1}, Loss: {loss:.4f}')
+    def update_parameters(self, gradients, learning_rate):
+        # Update parameters using gradients and learning rate
+        for i in range(self.num_layers):
+            self.weights[i] -= learning_rate * gradients[i][0]
+            self.biases[i] -= learning_rate * gradients[i][1]
 
+    def tanh(self, Z):
+        return np.tanh(Z)
 
-    def predict(self, X):
-        output = self.forward(X)
-        return np.argmax(output, axis=1)
-
+    def gradient_tanh(self, Z):
+        return 1 - np.tanh(Z)**2
 
 
 if __name__ == "__main__":
-    from sklearn.datasets import make_regression
-    from sklearn.model_selection import train_test_split
+    val = rand.random()
+    arr = [[0 for x in range(4)] for x in range(500)]
+    if val>0.5:
+        arr[0][0]=1
+    else:
+        arr[0][0]=-1
 
-    X, y = make_regression(n_samples=100, n_features=1, noise=0.1, random_state=42)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    X_train_mean = np.mean(X_train)
-    X_train_std = np.std(X_train)
-    X_train_std = (X_train - X_train_mean)/X_train_std
-    X_train = (X_test - X_train_mean)/X_train_std
+    print(arr)
 
-    y_train = y_train.reshape(-1,1)
-    y_test = y_test(-1,1)
+if __name__ == "main":
+    arr = np.array([[0,0], [1,0], [0,1], [0,0]])
+    arr2 = np.array([[0], [1], [1], [0]])
 
-    input_size = X_train.shape[1]
-    hidden_sizes = [10, 10]
-    output_size = y_train.shape[1]
-    mlp=MLP(input_size, hidden_sizes, output_size)
+    print(arr.T)
+    mlp=MLP(2, [3, 3],1)
 
-    num_epoch=1000
-    learning_rate=0.01
+    for epoch in range(100):
+        outputs = mlp.forward(arr.T)
+        gradients = mlp.backward(arr.T, arr2.T)
+        mlp.update_parameters(gradients, 0.01)
+        loss = np.mean((outputs - arr2.T)**2)
+        print(f"Epoch {epoch+1} - Loss: {loss}")
 
-    for epoch in range(num_epoch):
-        outputs = mlp.foward(X_train.T)
-        gradients = mlp.backward(X_train.T, y_train.T)
-        mlp.update_parameters(gradients, learning_rate)
+    test_output = mlp.forward(arr.T)
+    test_loss = np.mean((test_output - arr2.T) ** 2)
+    print(f"Test Loss: {test_loss}")
+    print(test_output)
